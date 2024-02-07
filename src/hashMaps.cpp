6f17c05894b20d0b7be7c00de121e9b6327e6a2a
kars_bj <bjarne.karsten@dlr.de>
2024-02-07 19:54:02 +0100

    Improve safety of registerRecipe

--- src/hashMaps.cpp
+++ src/hashMaps.cpp
@@ -8,6 +8,14 @@ namespace crafting {
     std::unordered_map<char32_t, std::shared_ptr<Character>> characterMap;
 
     bool registerRecipe(char32_t result, std::u32string recipeString) {
+        if(recipeString.find(U"？") != std::u32string::npos || recipeString.find(U"{") != std::u32string::npos) {
+            std::cerr << "Recipe contains unknown character." << std::endl;
+            return false;
+        }
+        if(recipeMap[recipeString]) {
+            std::cerr << "Recipe already registered." << std::endl;
+            return false;
+        }
         std::shared_ptr<Character> character;
         if(characterMap[result]) {
             character = characterMap[result];
