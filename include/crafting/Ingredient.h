37b84bce75afb8419605ac714b9450d3a37ac7cf
kars_bj <bjarne.karsten@dlr.de>
2024-03-14 20:30:39 +0100

    Add character-adding-methods to ingredient interface

--- include/crafting/Ingredient.h
+++ include/crafting/Ingredient.h
@@ -2,9 +2,13 @@
 #define INGREDIENT_H
 
 #include "Bitmap.h"
+#include "Character.h"
 #include <string>
+#include <memory>
 
 namespace crafting {
+
+class Character;
 
 /**
  * @brief An interface for components of a recipe.
@@ -17,6 +21,22 @@ public:
      * @brief Render the ingredient as a bitmap with the given dimensions.
     */
     virtual rendering::Bitmap render(int width, int height) const = 0;
+    /**
+     * @brief Add a character to the left of the ingredient.
+    */
+    virtual std::shared_ptr<Ingredient> addLeft(std::shared_ptr<Character> character) const = 0;
+    /**
+     * @brief Add a character to the right of the ingredient.
+    */	
+    virtual std::shared_ptr<Ingredient> addRight(std::shared_ptr<Character> character) const = 0;	
+    /**
+     * @brief Add a character above the ingredient.
+    */	
+    virtual std::shared_ptr<Ingredient> addAbove(std::shared_ptr<Character> character) const = 0;
+    /**
+     * @brief Add a character below the ingredient.
+    */		
+    virtual std::shared_ptr<Ingredient> addBelow(std::shared_ptr<Character> character) const = 0;	
 };
 
 } // namespace crafting
